ee041592ace95457ccb7c1bd2dde3882d2cd6b54
PedroCosta75 <69633049+PedroCosta75@users.noreply.github.com>
2021-11-01 21:23:21 +0000

    Add files via upload
    Done, need correcting issue

--- App.js
+++ App.js
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
 
 function App() {
-  /*
-  Define state variables for 
-  contacts and appointments 
-  */
+  const [appointments, setAppointments] = useState([]);
+  const [contacts, setContacts] = useState([]);
 
   const ROUTES = {
     CONTACTS: "/contacts",
     APPOINTMENTS: "/appointments",
   };
+  
+  const addAppointment = (title, contact, date, time) => {
+    setAppointments([
+      ...appointments,
+      {
+        title: title,
+        contact: contact,
+        date: date,
+        time: time,
+      },
+    ]);
+  };
 
-  /*
-  Implement functions to add data to
-  contacts and appointments
-  */
+  const addContact = (name, phone, email) => {
+    setContacts([
+      ...contacts,
+      {
+        name: name,
+        phone: phone,
+        email: email,
+      },
+    ]);
+  };
 
   return (
     <>
